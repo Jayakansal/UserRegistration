74bc230c1c3b2caaf928fada8a31465967df284e
Jayakansal <21jayajindal@gmail.com>
2020-12-01 14:46:30 +0530

    Added: Email_Address_logic

--- PatternExecise.sh
+++ PatternExecise.sh
@@ -3,10 +3,10 @@ echo "Welcome to the pattern execise"
 read -p "enter name:" Name
 firstName=(^[A-Z](a-z){2,}$)
 if(( $Name == $firstName ))
-then
-echo "yes"
+   then
+      echo "yes"
 else
-echo "no"
+      echo "no"
 fi
 
 echo "*********************************"
@@ -14,8 +14,20 @@ echo "*********************************"
 read -p "enter name:" LastName
 Lastname=(^[A-Z](a-z){2,}$)
 if(( $LastName == $Lastname ))
-then
-echo "yes"
+   then
+
+     echo "yes"
 else
-echo "no"
+     echo "no"
+fi
+
+echo "*********************************"
+
+read -p "enter email address:" email
+emailpat="^[0-9a-zA-Z]+([.\-_+][0-9a-zA-z]+)*@[0-9a-zA-z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
+if (( $email == $emailpat ))
+then
+	echo "yes"
+else
+	echo "no"
 fi
