8b31e4c80a367abd31c7349e1e3e96b8685b2814
Jayakansal <21jayajindal@gmail.com>
2020-12-01 14:40:36 +0530

    Added:Last_Name_Logic

--- PatternExecise.sh
+++ PatternExecise.sh
@@ -1,8 +1,19 @@
-#!/bin/bah
-echo"welcome to pattern execise"
+#!/bin/bash
+echo "Welcome to the pattern execise"
 read -p "enter name:" Name
-firstName=(^[A-Z](a-z){2})
+firstName=(^[A-Z](a-z){2,}$)
 if(( $Name == $firstName ))
+then
+echo "yes"
+else
+echo "no"
+fi
+
+echo "*********************************"
+
+read -p "enter name:" LastName
+Lastname=(^[A-Z](a-z){2,}$)
+if(( $LastName == $Lastname ))
 then
 echo "yes"
 else
